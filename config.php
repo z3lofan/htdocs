9a08ad7031c94ba5e9036bd15b67fe335dd62473
z3lofan <gonty11@hotmail.com>
2015-01-30 15:04:35 +0100

    Base
    Base del programa

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+if(!defined("INSIDE")){ header("location:./../"); }
+$dbsettings = Array(
+"server"     => "localhost", // MySQL server name.
+"user"       => "root", // MySQL username.
+"pass"       => "", // MySQL password.
+"name"       => "Univer", // MySQL database name.
+"prefix"     => "xgp_", // Tables prefix.
+"secretword" => "XGProyect538549142"); // Cookies.
+?>
